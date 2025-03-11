33887290bd4cc4ab3250f0f7ba8d456f9adc5527
mopoy <mopoy.code@gmail.com>
2025-03-11 16:24:33 +0330

    feat: add nack to consumer

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="proker",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(where="src"),
     include_package_data=True,
     package_dir={"": "src"},
     description="a dependency inversion python package for using rabbit-mq and kafka",
     author="mopoy",
     author_email="mopoy.code@gmail.com",
-    url="https://github.com/yourusername/my_package",  # Optional
+    url="https://github.com/sudomopoy/proker-py",  # Optional
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,9 +20,5 @@ setup(
         "https://pypi.org/project/pika/",
         "https://pypi.org/project/kafka-python/",
     ],
-    install_requires=[
-        "pika>=1.1.0",
-        "kafka-python>=2.0.2",
-        "protobuf>=6.30.0"
-    ],
+    install_requires=["pika>=1.1.0", "kafka-python>=2.0.2", "protobuf>=6.30.0"],
 )
--- src/proker/rabbit/consumer.py
+++ src/proker/rabbit/consumer.py
@@ -8,12 +8,19 @@ from proker.serializer import BaseSerializer, JSONSerializer
 from proker.core import BaseConsumer
 from typing import Any, Callable, Dict, Optional
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class RabbitMQConsumer(BaseConsumer):
-    def __init__(self, config: Dict, retry_policy: RetryPolicy, serializer: BaseSerializer = JSONSerializer()):
+    def __init__(
+        self,
+        config: Dict,
+        retry_policy: RetryPolicy,
+        serializer: BaseSerializer = JSONSerializer(),
+    ):
         super().__init__()
         self.serializer = serializer
         self.config = config
@@ -36,17 +43,25 @@ class RabbitMQConsumer(BaseConsumer):
         )
         self.channel = self.connection.channel()
         self._declare_infrastructure()
-        
+
     @auto_reconnect
-    def consume(self,callback: Callable[[Any], None], queue_name: str = '', auto_ack: bool = True):
-        queue = queue_name or self.config.get('queue_name', '')
+    def consume(
+        self,
+        callback: Callable[[Any], bool],
+        queue_name: str = "",
+        auto_ack: bool = True,
+        backoff_factor: float = 1.0,
+    ):
+        queue = queue_name or self.config.get("queue_name", "")
         self.channel.basic_consume(
             queue=queue,
-            on_message_callback=lambda ch, method, properties, body: self._handle_message(ch, method, properties, body, callback),
-            auto_ack= auto_ack or self.config.get('auto_ack', True)
+            on_message_callback=lambda ch, method, properties, body: self._handle_message(
+                ch, method, properties, body, callback, backoff_factor
+            ),
+            auto_ack=auto_ack or self.config.get("auto_ack", True),
         )
         self.channel.start_consuming()
- 
+
     def _declare_infrastructure(self):
         exchange = self.config.get("exchange")
         if exchange:
@@ -56,14 +71,13 @@ class RabbitMQConsumer(BaseConsumer):
                 durable=exchange.get("durable", True),
             )
 
-
         queue = self.config.get("queue")
         if queue:
             self.declare_queue(
                 queue_name=queue["name"],
-                durable=queue.get("durable",True),
+                durable=queue.get("durable", True),
             )
-            
+
             if exchange:
                 self.bind_queue(
                     exchange_name=exchange["name"],
@@ -71,7 +85,17 @@ class RabbitMQConsumer(BaseConsumer):
                     routing_key=queue.get("routing_key", "#"),
                 )
 
-    def declare_exchange(self, exchange_name: str, exchange_type: str = "topic", durable: bool = True, auto_delete: bool = False, internal: bool = False, passive: bool = False, headers: Dict = None, message_ttl: int = None,):
+    def declare_exchange(
+        self,
+        exchange_name: str,
+        exchange_type: str = "topic",
+        durable: bool = True,
+        auto_delete: bool = False,
+        internal: bool = False,
+        passive: bool = False,
+        headers: Dict = None,
+        message_ttl: int = None,
+    ):
         self.channel.exchange_declare(
             exchange=exchange_name,
             exchange_type=exchange_type,
@@ -80,37 +104,64 @@ class RabbitMQConsumer(BaseConsumer):
             internal=internal,
             passive=passive,
         )
-    def declare_queue(self, queue_name: str, durable: bool = True, no_ack: bool = False, exclusive: bool = False, auto_delete: bool = False, arguments: Dict = None):
+
+    def declare_queue(
+        self,
+        queue_name: str,
+        durable: bool = True,
+        no_ack: bool = False,
+        exclusive: bool = False,
+        auto_delete: bool = False,
+        arguments: Dict = None,
+    ):
         self.channel.queue_declare(
-            queue=queue_name, 
+            queue=queue_name,
             durable=durable,
             exclusive=exclusive,
             auto_delete=auto_delete,
             arguments=arguments,
         )
+
     def bind_queue(self, exchange_name: str, queue_name: str, routing_key: str = "#"):
         self.channel.queue_bind(
             exchange=exchange_name,
             queue=queue_name,
-            routing_key= routing_key,
+            routing_key=routing_key,
         )
 
-                
     def _handle_message(self, channel, method, properties, body, callback):
         try:
             message = self.serializer.unmarshal(body)
-            callback(message)
-            if not self.config.get('auto_ack', True):
-                channel.basic_ack(delivery_tag=method.delivery_tag)
+            if callback(message):
+                if not self.config.get("auto_ack", True):
+                    channel.basic_ack(delivery_tag=method.delivery_tag)
+            else:
+                if "x-retry-count" in properties.headers:
+                    properties.headers["x-retry-count"] += 1
+                else:
+                    properties.headers["x-retry-count"] = 1
+                time.sleep(backoff_factor)
+                # Nack the message and requeue it
+                channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+                # Optionally, you can log the error or take other actions
+                logger(
+                    f"Message nacked. Error: {e}. Retry count: {properties.headers['x-retry-count']}"
+                )
+
         except Exception as e:
             logger.error(f"Error processing message: {str(e)}")
-            if not self.config.get('auto_ack', True):
+            if not self.config.get("auto_ack", True):
                 channel.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
-            
+
     def is_connected(self) -> bool:
         try:
-            return bool(self.connection and self.connection.is_open 
-                     and self.channel and self.channel.is_open)
+            return bool(
+                self.connection
+                and self.connection.is_open
+                and self.channel
+                and self.channel.is_open
+            )
         except Exception as e:
             logger.debug(f"Connection check failed: {str(e)}")
             return False
